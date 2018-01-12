d2b6bbd7a64d52451096d877eba6c93c9779c26a
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-01-12 20:08:17 +0200

    commit

---
+++ lesson 4 - 29.12.2017/src/Classwork.java
@@ -0,0 +1,66 @@
+/**
+ * Created by java on 29.12.2017.
+ */
+public class Classwork {
+
+////  1 вывести строку str но заменит все буквы В на :)
+//    //  вы вести каждый каждый четный символ
+//    //  вывети кол-во повторов слова альбом
+//
+    public static void main(String[] args) {
+//        String str = new String(" Был и я среди донцов...\n" +
+//                "        Была пора: наш праздник молодой...\n" +
+//                "        В альбом (Гонимый рока самовластьем...)\n" +
+//                "        В альбом (Долго сих листов заветных...)\n" +
+//                "        В альбом А. О. Смирновой\n" +
+//                "        В альбом княжны А. Д. Абамалек\n" +
+//                "        В альбом Павлу Вяземскому\n" +
+//                "        В еврейской хижине лампада...\n" +
+//                it status
+// "        В крови горит огонь желанья...\n" +
+//                "        В мои осенние досуги...\n" +
+//                "        В начале жизни школу помню я...\n" +
+//                "        В поле чистом серебрится Снег...");
+//        System.out.print("Result in char: ");
+//        System.out.println(str.toCharArray());
+//        System.out.println(str.replace('В', ':'));
+
+
+//        int i;
+//        System.out.println("Enter the number");
+//        Scanner scn = new Scanner (System.in);
+//        i = scn.nextInt();
+//        switch (i)
+//        {
+//            case 1:
+//                System.out.println();
+//                break;
+//            case 2:
+//                System.out.println();
+//            case 3:
+//                System.out.println();
+
+
+        firstMethod();
+        }
+    public static void firstMethod(){
+        int a = 2;
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+
+    public static String firstStringMethod(){
+
+        return null;
+    }
+
+    }
+
+
+
+
+
+
+
+
