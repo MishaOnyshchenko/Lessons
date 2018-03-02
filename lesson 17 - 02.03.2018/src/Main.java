ceccbfb5924aeffdc9b743bfff4806f30cef6725
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-03-02 20:53:08 +0200

    02.03 added

--- lesson 17 - 02.03.2018/src/Main.java
+++ lesson 17 - 02.03.2018/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -14,6 +16,29 @@ public class Main {
         dogMass[1] = null;
         for (Dog dogMas : dogMass){
             System.out.println(dogMas);
+        }
+
+        ArrayList<Dog> dogisList = new ArrayList<>();
+        dogisList.add(dog);
+        dogisList.add(dogic);
+        dogisList.add(new Dog ("volic",8));
+        dogisList.add(new Dog ("tamara",15));
+
+        for (Dog dog1: dogisList) {
+            System.out.println(dog1.getName());
+        }
+
+        System.out.println(dogisList.contains(dogic));
+
+        List newList = new ArrayList<>();
+
+        newList.add(dog);
+        newList.add(muric);
+
+        for (Object o: newList) {
+            if (o instanceof Cat){
+                System.out.println(((Cat) o).getName());
+            }
         }
 
     }
