f1fa9342ac4d174f59f5f67eabff913b43b707f1
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-04-10 20:31:01 +0300

    10.04

---
+++ lesson 25 - 10.04.2018/src/ATM.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ATM extends Thread {
+
+    private double balance = 1200;
+    private String currency = "UAH";
+    private Lock lock = new ReentrantLock();
+
+    public void getMoney(double money) {
+
+        lock.lock();
+        try {
+            if (balance - money > 0) {
+                balance -= money;
+                System.out.println("You have " + money);
+            } else if (balance - money < 0) {
+                System.out.println("You don't have enough money. Your balance is " + balance);
+            } else {
+                System.out.println("You are empty");
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public ATM(double money) {
+        getMoney(money);
+    }
+    public ATM() {
+    }
+
+}
---
+++ lesson 25 - 10.04.2018/src/RunAtm.java
@@ -0,0 +1,31 @@
+
+public class RunAtm {
+    public static void main(String[] args) {
+
+        ATM my = new ATM(400);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                my.getMoney(300);
+            }
+        };
+        thread.start();
+
+        Thread thread2 = new Thread(){
+            @Override
+            public void run(){
+                my.getMoney(300);
+            }
+        };
+        thread2.start();
+
+        Thread thread3 = new Thread(){
+            @Override
+            public void run(){
+                my.getMoney(700);
+            }
+        };
+        thread3.start();
+    }
+}
