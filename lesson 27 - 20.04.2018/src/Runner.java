02b0fced7c760fa2db580024107790472864cc32
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-04-20 20:54:10 +0300

    finished fr today

--- lesson 27 - 20.04.2018/src/Runner.java
+++ lesson 27 - 20.04.2018/src/Runner.java
@@ -1,3 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Runner {
+    public static void main(String[] args) {
+        String input = "Misha says hello, misha! OkMisha";
+        Pattern pattern = Pattern.compile("Misha(\\w*)");
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find())
+            System.out.println(matcher.group());
+    }
 }
