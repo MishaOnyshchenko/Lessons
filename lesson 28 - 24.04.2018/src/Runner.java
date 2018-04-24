cd37b843cd9e0d51e8bc7375b17f6e691752fb09
stasyabezruk <stasyabezruk@ukr.net>
2018-04-24 18:51:12 +0300

    test

---
+++ lesson 28 - 24.04.2018/src/Runner.java
@@ -0,0 +1,13 @@
+import lombok.Data;
+
+@Data
+public class Runner {
+
+    public static void main(String[] args) {
+
+        User user = new User();
+        System.out.println(user
+        );
+
+    }
+}
