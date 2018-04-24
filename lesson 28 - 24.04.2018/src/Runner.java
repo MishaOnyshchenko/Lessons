42812581c0a2ec6a7a939a83ecc2ee0e7b6891fb
stasyabezruk <stasyabezruk@ukr.net>
2018-04-24 18:56:44 +0300

    new

--- lesson 28 - 24.04.2018/src/Runner.java
+++ lesson 28 - 24.04.2018/src/Runner.java
@@ -1,13 +1,10 @@
-import lombok.Data;
 
-@Data
 public class Runner {
 
     public static void main(String[] args) {
 
         User user = new User();
-        System.out.println(user
-        );
+        user.setAge(44);
 
     }
 }
