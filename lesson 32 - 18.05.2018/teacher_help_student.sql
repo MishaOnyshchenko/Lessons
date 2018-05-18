6b86ed40fee22dc584317dccb222b68d08c6bb37
MishaOnyshchenko <misha.onyshchenko@gmail.com>
2018-05-18 20:56:11 +0300

    upd

---
+++ lesson 32 - 18.05.2018/teacher_help_student.sql
@@ -0,0 +1,20 @@
+CREATE TABLE MY_TABLE (
+  username VARCHAR,
+  sec_name VARCHAR,
+  age INT
+);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud1', 'st1', 15);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud1', 'st1', 15);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud2', 'st2', 16);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud3', 'st3', 17);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud1', 'st1', 15);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud3', 'st3', 17);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud3', 'st3', 17);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('team', 'timon', null);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('team', 'timon', null);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud1', 'st1', 14);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud2', 'st2', 16);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud3', 'st3', 18);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud1', 'st1', 14);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud2', 'st2', 16);
+INSERT INTO MY_TABLE(username, sec_name, age) VALUES ('stud3', 'st3', 18);
